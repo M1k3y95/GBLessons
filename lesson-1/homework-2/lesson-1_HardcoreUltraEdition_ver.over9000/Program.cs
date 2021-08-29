c2436df29a2de010995216c06e3ef711b3da94a0
M1k3y95 <rockheart95@gmail.com>
2021-08-29 05:08:52 +0500

    Lesson-1 homework + challenge from the teacher

---
+++ lesson-1/homework-2/lesson-1_HardcoreUltraEdition_ver.over9000/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace lesson_1_HardcoreUltraEdition_ver.over9000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = new Stopwatch();
+
+            sw.Start();
+
+            ulong iterationCount = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                iterationCount++;
+                if (i == 100)
+                {
+                    Console.Write("buzz");
+                    break;
+                }
+
+                if (i % 3 == 0 && i % 5 ==0)
+                {
+                    Console.Write("fizz buzz, ");
+                    Console.Write($"{i + 1}, {i + 2}, ");
+                    i += 2;
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.Write("fizz, ");
+                    //if ((i + 1) % 5 != 0)
+                    //{
+                    //    i++;
+                    //    Console.Write($"{i}, ");
+                    //}
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.Write("buzz, ");
+                    //if ((i + 1) % 3 != 0)
+                    //{
+                    //    i++;
+                    //    Console.Write($"{i}, ");
+                    //}
+                }
+                else
+                {
+                    Console.Write($"{i}, ");
+                }
+                
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Количество итераций: {iterationCount}");
+            sw.Stop();
+            Console.WriteLine($"Time Spent: {sw.ElapsedMilliseconds}");
+        }
+    }
+}
