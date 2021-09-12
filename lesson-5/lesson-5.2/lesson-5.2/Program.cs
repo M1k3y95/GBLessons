b1ccf27d3ccb9074b0b0ea10136a00aadd2528a2
M1k3y95 <rockheart95@gmail.com>
2021-09-12 22:35:43 +0500

    lesson-5 homework, 4/5. I need more time for the last task

---
+++ lesson-5/lesson-5.2/lesson-5.2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace lesson_5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string date = $"{DateTime.Now} \n";
+
+            File.AppendAllText(@"D:\GeekBrains_Lessons\HomeWork\lesson-5\lesson-5.2\startup.txt", date);
+        }
+    }
+}
---
+++ lesson-5/lesson-5.4/lesson-5.4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace lesson_5._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\GeekBrains_Lessons\HomeWork";
+
+            PrintDirectories(path);
+        }
+
+        private static void PrintDirectories(string path, string prefix = "", string txtFilePath = @"D:\GeekBrains_Lessons\HomeWork\lesson-5\lesson-5.4\DirectoryTree.txt")
+        {
+            var startDir = new DirectoryInfo(path);
+            var treeItems = startDir.GetFileSystemInfos();
+
+                for (int i = 0; i < treeItems.Length; i++)
+                {
+                    var treeItem = treeItems[i];
+                    if (i == treeItems.Length - 1)
+                    {
+                        File.AppendAllText(txtFilePath, prefix + "└── " + treeItem.Name + "\n", encoding:System.Text.Encoding.Unicode);
+
+                        if ((treeItem.Attributes & FileAttributes.Directory) == FileAttributes.Directory)
+                        {
+                            PrintDirectories(treeItem.FullName, prefix + "      ");
+                        }
+                    }
+                    else
+                    {
+                        File.AppendAllText(txtFilePath, prefix + "├── " + treeItem.Name + "\n", encoding: System.Text.Encoding.Unicode);
+
+                        if ((treeItem.Attributes & FileAttributes.Directory) == FileAttributes.Directory)
+                        {
+                            PrintDirectories(treeItem.FullName, prefix + "|    ");
+                        }
+                    }
+
+            }
+        }
+    }
+}
